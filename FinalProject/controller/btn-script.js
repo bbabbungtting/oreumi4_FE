6cd970e979e9b6ec62892c85de6f911717c3e9a4
Jaewon <choijawon99@gmail.com>
2024-01-18 17:18:05 +0900

    [FEAT] 모달 기능 추가

--- FinalProject/controller/btn-script.js
+++ FinalProject/controller/btn-script.js
@@ -1,4 +1,7 @@
 const btn_show = document.querySelector('.button-show');
+const btn_sub = document.querySelector(".button-subscribe");
+const btn_love = document.querySelector('.button-love');
+const modal = document.getElementById('modal');
 let pageNum = 1;
 
 btn_show.addEventListener('click', function () {
@@ -11,3 +14,11 @@ btn_show.addEventListener('click', function () {
         fetchImages(pageNum);
     }
 });
+
+btn_sub.addEventListener('click',function(){
+    modal.style.display = 'block';
+})
+
+btn_love.addEventListener('click',function (){
+    modal.style.display = 'none';
+})
