e640b66e3bdc19ad777c1df6b326ed764f860410
Jaewon <choijawon99@gmail.com>
2024-01-18 17:55:38 +0900

    [FEAT] 위로 가기 버튼 완성

--- FinalProject/controller/btn-script.js
+++ FinalProject/controller/btn-script.js
@@ -1,6 +1,8 @@
 const btn_show = document.querySelector('.button-show');
 const btn_sub = document.querySelector(".button-subscribe");
 const btn_love = document.querySelector('.button-love');
+const btn_up = document.getElementById('botton-up');
+let img_up = document.getElementById('img-up');
 const modal = document.getElementById('modal');
 let pageNum = 1;
 
@@ -22,3 +24,26 @@ btn_sub.addEventListener('click',function(){
 btn_love.addEventListener('click',function (){
     modal.style.display = 'none';
 })
+
+
+var backToTop = () =>{
+    window.addEventListener('scroll',()=>{
+        if(document.querySelector('html').scrollTop>10){
+            btn_up.style.borderColor = '#999999';
+            img_up.src = "../assets/images/arrow_can_up.svg";
+        }else{
+            btn_up.style.borderColor = '#D3D3D3';
+            img_up.src = "../assets/images/arrow_cant_up.svg";
+        }
+    })
+
+    btn_up.addEventListener('click',()=>{
+        window.scrollTo({
+            top:0,
+            left:0,
+            behavior:'smooth'
+        });
+    })
+};
+
+backToTop();
