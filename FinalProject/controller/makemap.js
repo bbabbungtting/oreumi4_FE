83446c5bf3a8ef17caabe906f3b04c9e276f2153
Jaewon <choijawon99@gmail.com>
2024-01-22 10:31:57 +0900

    [FEAT] 지도 API 추가
    - 컨트롤 버튼 추가
    - 기본 위치 마커 생성

--- FinalProject/controller/makemap.js
+++ FinalProject/controller/makemap.js
@@ -1,7 +1,43 @@
-var mapContainer = document.getElementById('kakaomap'),
+var mapContainer = document.getElementById('kakaomap'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(33.4423379727783, 126.571449734542),
-        level: 3
+        center: new kakao.maps.LatLng(33.4423379727783, 126.571449734542), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
     };
 
-var map = new kakao.maps.Map(mapContainer, mapOption);
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
+function setMapType(maptype) {
+    var roadmapControl = document.getElementById('btnRoadmap');
+    var skyviewControl = document.getElementById('btnSkyview');
+    if (maptype === 'roadmap') {
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+        roadmapControl.className = 'selected_btn';
+        skyviewControl.className = 'btn';
+    } else {
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+        skyviewControl.className = 'selected_btn';
+        roadmapControl.className = 'btn';
+    }
+}
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+    map.setLevel(map.getLevel() - 1);
+}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+    map.setLevel(map.getLevel() + 1);
+}
+
+// 마커가 표시될 위치입니다
+var markerPosition  = new kakao.maps.LatLng(33.4423379727783, 126.571449734542);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
